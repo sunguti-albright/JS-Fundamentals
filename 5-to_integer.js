37597fa11a0923aede062931e985dd36c469c212
sunguti-albright <sungutialbright@gmail.com>
2025-06-20 14:12:28 +0300

    task 5

--- 5-to_integer.js
+++ 5-to_integer.js
@@ -6,3 +6,33 @@ You must use console.log(...) to print all output
 You are not allowed to use var
 You are not allowed to use try/catch */
 
+
+const arg = process.argv[2];   // always a string from CLI
+const num = Number(arg);       // try to convert
+
+if (!Number.isNaN(num)) {
+  console.log("My number: " + num);
+} else {
+  console.log("Not a number");
+}
+
+
+
+// const argu = process.argv[2];      // first argument
+// const numb = Number(arg);          // convert to number (strict)
+
+// if (Number.isNaN(numb)) {
+//   console.log("Not a number");
+// } else {
+//   console.log("My number: " + numb);
+// }
+
+
+// const args = process.argv[2];          // first argument
+// const nums = parseInt(arg, 10);        // convert to integer (base 10)
+
+// if (isNaN(nums)) {
+//   console.log("Not a number");
+// } else {
+//   console.log("My number: " + nums);
+// }
