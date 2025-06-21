72f773b9f9d1425b23d2acafa6af0ad43bcec754
sunguti-albright <sungutialbright@gmail.com>
2025-06-21 13:59:56 +0300

    task 5

---
+++ 5-to_integer.js
@@ -0,0 +1,8 @@
+/*
+Write a script that prints My number: <first argument converted in integer> if the first argument can be converted to an integer:
+
+If the argument can’t be converted to an integer, print “Not a number”
+You must use console.log(...) to print all output
+You are not allowed to use var
+You are not allowed to use try/catch */
+
