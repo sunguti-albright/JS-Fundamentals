3ab7725aa0f180ea8e1d7c6d773f91900b59bc4e
sunguti-albright <sungutialbright@gmail.com>
2025-06-29 12:19:16 +0300

    task:script that prints 3 lines

---
+++ 1-multi_languages.js
@@ -0,0 +1,12 @@
+/*
+Write a script that prints 3 lines:
+
+The first line: “C is fun”
+The second line: “Python is cool”
+The third line: “JavaScript is amazing”
+You must use console.log(...) to print all output
+You are not allowed to use var*/
+
+console.log("C is fun")
+console.log("Python is cool")
+console.log("JavaScript is amazing")
