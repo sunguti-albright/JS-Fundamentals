a68d7d527188da75f47f654fbf835909e7f6232b
sunguti-albright <sungutialbright@gmail.com>
2025-09-09 13:39:38 +0300

    cocatenation

---
+++ 4-concat.js
@@ -0,0 +1,9 @@
+/*
+Write a script that prints two arguments passed to it, in the following format: “ is ”
+
+You must use console.log(...) to print all output
+You are not allowed to use var*/
+
+let arg1 = process.argv[2];
+let arg2 = process.argv[3];
+console.log (arg1 + " is " + arg2);
